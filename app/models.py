9bdbb2be5138dcf7bb098659911573f72da3bb7f
manskezada <christian_manske@estudante.sesisenai.org.br>
2025-07-02 09:13:19 -0300

    Adição de db

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from app import db
 from datetime import datetime
 
-class contato(db.Model):
+class Contato(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    data_envio = db.Column(db.DateTime,default=datetime.now())
+    data_envio = db.Column(db.DateTime,default=datetime.now)
     nome = db.Column(db.String,nullable=True)
     email = db.Column(db.String,nullable=True)
     assunto = db.Column(db.String,nullable=True)
